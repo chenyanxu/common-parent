47a78e8a48889ad9d5136667c98d3b3e8a853144
sunlingfeng <minikiller@qq.com>
2016-08-24 09:29:49 +0800

    modify dict and etc module

--- system-parent/dict-parent/common-system-dict-api/src/main/java/com/kalix/common/system/dict/api/biz/ICommonDictBeanService.java
+++ system-parent/dict-parent/common-system-dict-api/src/main/java/com/kalix/common/system/dict/api/biz/ICommonDictBeanService.java
@@ -1,11 +1,8 @@
 package com.kalix.common.system.dict.api.biz;
 
-import com.kalix.framework.core.api.biz.IBizService;
 import com.kalix.common.system.dict.entities.CommonDictBean;
+import com.kalix.framework.core.api.system.IDictBeanService;
 
-import java.util.List;
-import java.util.Map;
 
-public interface ICommonDictBeanService extends IBizService<CommonDictBean> {
-    List<Map> getDictTypes(String query);
+public interface ICommonDictBeanService extends IDictBeanService<CommonDictBean> {
 }
