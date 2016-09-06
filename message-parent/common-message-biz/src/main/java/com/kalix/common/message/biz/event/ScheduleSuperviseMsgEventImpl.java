705a6695f26f73f22304e77f9b35e3e514f2f6e0
zangyanming <zyanming2002@163.com>
2016-09-06 14:32:55 +0800

    务督办消息处理格式修改

--- message-parent/common-message-biz/src/main/java/com/kalix/common/message/biz/event/ScheduleSuperviseMsgEventImpl.java
+++ message-parent/common-message-biz/src/main/java/com/kalix/common/message/biz/event/ScheduleSuperviseMsgEventImpl.java
@@ -13,7 +13,7 @@ import org.osgi.service.event.EventHandler;
  * Created by sunlf on 2016/8/26.
  */
 public class ScheduleSuperviseMsgEventImpl extends BaseMessageEvent implements EventHandler {
-    public static final String MSG_CONTENT = "%s,您好！\r\n  您收到了一条由%s督办的消息，任务名称为:%s，请查看！";
+    public static final String MSG_CONTENT = "%s,您好！\r\n  您收到了一条由%s督办的消息，任务名称为:%s，%s！";
     public static final String MSG_TITLE = "计划任务督办提醒";
 
     @Override
@@ -26,8 +26,8 @@ public class ScheduleSuperviseMsgEventImpl extends BaseMessageEvent implements E
         String userName = (String) event.getProperty("userName");
         //任务名称
         String taskName = (String) event.getProperty("taskName");
-
-        String content = String.format(MSG_CONTENT, headName, userName, taskName);
+        String info = (String)event.getProperty("info");
+        String content = String.format(MSG_CONTENT, headName, userName, taskName,info);
 
         MessageBean messageBean = createMessageBean(userBean.getId(), content, MSG_TITLE);
         dao.save(messageBean);
