f65bc9fa90dc8bc5df81152fc530cda18c58d660
paning <pwcn_78@163.com>
2016-09-19 08:02:15 +0800

    修改分隔符造成样式串问题

--- message-parent/common-message-extjs/src/main/webapp/message/sender/view/MessageSearchForm.js
+++ message-parent/common-message-extjs/src/main/webapp/message/sender/view/MessageSearchForm.js
@@ -43,16 +43,15 @@ Ext.define('kalix.message.sender.view.MessageSearchForm', {
             format: 'Y-m-d',
             fieldLabel: '发件时间:',
             labelAlign: 'right',
-            labelWidth: 120,
-            width: 260,
+            labelWidth: 60,
+            width: 200,
             margin: '0 0 0 20',
             name: 'creationDate:begin:gt'
         },
         {
-            xtype: 'displayfield',
-            hideLabel: true,
-            value: '-',
-            margin: '0 5 0 5'
+            xtype: 'label',
+            text: '-',
+            margin: '5 5 0 5'
         },
         {
             xtype: 'datefield',
