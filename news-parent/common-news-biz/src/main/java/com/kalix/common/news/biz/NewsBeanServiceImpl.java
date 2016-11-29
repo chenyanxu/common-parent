8f7bf147bfc658485f7c0cb062cd557b543391b2
chenyanxu <yanxu2016>
2016-11-29 16:53:27 +0800

    change the base class of the NewsBeanServiceImpl,update the xml

--- news-parent/common-news-biz/src/main/java/com/kalix/common/news/biz/NewsBeanServiceImpl.java
+++ news-parent/common-news-biz/src/main/java/com/kalix/common/news/biz/NewsBeanServiceImpl.java
@@ -5,6 +5,7 @@ import com.kalix.common.news.api.biz.INewsBeanService;
 import com.kalix.common.news.api.dao.INewsBeanDao;
 import com.kalix.common.news.entities.NewsBean;
 import com.kalix.framework.core.api.persistence.JsonStatus;
+import com.kalix.framework.core.impl.biz.ShiroGenericBizServiceImpl;
 import com.kalix.framework.core.util.Assert;
 import org.apache.commons.lang.StringUtils;
 
@@ -19,44 +20,5 @@ import java.util.Date;
  * @修改时间：
  * @修改备注：
  */
-public class NewsBeanServiceImpl extends AuditBizServiceImpl<INewsBeanDao, NewsBean> implements INewsBeanService {
-    private static final String APPLICATION_ALIAS = "我的首页";
-    private static final String NEWS_MENU_TEXT = "公司新闻";
-    private JsonStatus jsonStatus = new JsonStatus();
-//    private IShiroService shiroService;
-
-    @Override
-    @TransactionScoped
-    public void beforeSaveEntity(NewsBean entity, JsonStatus status) {
-        //添加时候，记录操作人和时间
-//        if (entity.getId() == -1) {
-        String userName = shiroService.getCurrentUserRealName();
-        Assert.notNull(userName, "用户名不能为空.");
-        if (StringUtils.isNotEmpty(userName)) {
-            entity.setPublishPeople(userName);
-            entity.setPublishDate(new Date());
-        }
-        super.beforeSaveEntity(entity, status);
-//        throw new RuntimeException("test");
-//        }
-    }
-
-
-    public NewsBeanServiceImpl() {
-        super.init(NewsBean.class.getName());
-    }
-
-//    public void setShiroService(IShiroService shiroService) {
-//        this.shiroService = shiroService;
-//    }
-
-    @Override
-    public String getAppName() {
-        return APPLICATION_ALIAS;
-    }
-
-    @Override
-    public String getFunName() {
-        return NEWS_MENU_TEXT;
-    }
+public class NewsBeanServiceImpl extends ShiroGenericBizServiceImpl<INewsBeanDao, NewsBean> implements INewsBeanService {
 }
