bb4602e2b7f5034546f58560296d44cb17e16d42
sunlingfeng <minikiller@qq.com>
2017-08-19 19:30:28 +0800

    change biz methods

--- news-parent/common-news-biz/src/main/java/com/kalix/common/news/biz/NewsBeanServiceImpl.java
+++ news-parent/common-news-biz/src/main/java/com/kalix/common/news/biz/NewsBeanServiceImpl.java
@@ -1,6 +1,5 @@
 package com.kalix.common.news.biz;
 
-import com.kalix.admin.audit.biz.AuditBizServiceImpl;
 import com.kalix.common.news.api.biz.INewsBeanService;
 import com.kalix.common.news.api.dao.INewsBeanDao;
 import com.kalix.common.news.entities.NewsBean;
@@ -9,7 +8,6 @@ import com.kalix.framework.core.impl.biz.ShiroGenericBizServiceImpl;
 import com.kalix.framework.core.util.Assert;
 import org.apache.commons.lang.StringUtils;
 
-import javax.transaction.TransactionScoped;
 import java.util.Date;
 
 /**
@@ -21,4 +19,13 @@ import java.util.Date;
  * @修改备注：
  */
 public class NewsBeanServiceImpl extends ShiroGenericBizServiceImpl<INewsBeanDao, NewsBean> implements INewsBeanService {
+    @Override
+    public void beforeSaveEntity(NewsBean entity, JsonStatus status) {
+        String userName = shiroService.getCurrentUserRealName();
+        Assert.notNull(userName, "用户名不能为空.");
+        if (StringUtils.isNotEmpty(userName)) {
+            entity.setPublishPeople(userName);
+            entity.setPublishDate(new Date());
+        }
+    }
 }
