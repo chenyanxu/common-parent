95d1afb9560f3aff2d38b2ccf5d6c0476d5df00f
minikiller <minikiller@qq.com>
2018-06-04 14:53:01 +0800

    add websocket client

---
+++ message-parent/common-message-biz/src/main/java/com/kalix/common/message/biz/servlet/Client.java
@@ -0,0 +1,41 @@
+package com.kalix.common.message.biz.servlet;
+
+import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
+import org.eclipse.jetty.websocket.client.WebSocketClient;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Administrator on 2018/6/4.
+ */
+public class Client {
+    public static void main(String[] args) {
+        String destUri = "ws://localhost:8181/websocket";
+        if (args.length > 0) {
+            destUri = args[0];
+        }
+
+        WebSocketClient client = new WebSocketClient();
+        KarafWebSocket socket = new KarafWebSocket();
+        try {
+            client.start();
+
+            URI echoUri = new URI(destUri);
+            ClientUpgradeRequest request = new ClientUpgradeRequest();
+            client.connect(socket, echoUri, request);
+            System.out.printf("Connecting to : %s%n", echoUri);
+
+            // wait for closed socket connection.
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+        } finally {
+            try {
+                client.stop();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
